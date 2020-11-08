153fc62c6ac2719e23464a8ad9c1a64b8d7fb5dc
kesav kumar <43528483+kesavsivakumar@users.noreply.github.com>
2020-11-08 14:28:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -101,9 +101,9 @@ def make_prediction():
         
         option = request.form['options']
         if option=='option1':
-                output=H1_H2_H3(inp,text)
+                output=H1_H2_H3(text)
         elif option=='option2':
-                output=model2(inp,text)
+                output=model2(text)
         elif option ==None:
                 return render_template_string('the text could not be classified into any othe given fields please try click any of the models mentioned')
 
